f4a90d9369e970b9fab98d106b349c44b89b7773
Vinodbiradar09 <vinodjb07@gmail.com>
2025-10-05 03:13:58 +0530

    t3

--- src/app/mail/components/sidebar.tsx
+++ src/app/mail/components/sidebar.tsx
@@ -1,65 +1,72 @@
-"use client";
+'use client'
 import React from 'react'
+import { Nav } from './nav'
+
 import {
+    AlertCircle,
+    Archive,
+    ArchiveX,
     File,
     Inbox,
+    MessagesSquare,
     Send,
+    ShoppingCart,
+    Trash2,
+    Users2,
 } from "lucide-react"
-import { Nav } from './nav'
+import { usePathname } from 'next/navigation'
 import { useLocalStorage } from 'usehooks-ts'
-import { api } from '@/trpc/react';
-
-
-
+import { api } from '@/trpc/react'
 type Props = { isCollapsed: boolean }
-const SideBar = ({isCollapsed} : Props)=>{
+
+const SideBar = ({ isCollapsed }: Props) => {
 
     const [tab] = useLocalStorage("normalhuman-tab", "inbox")
     const [accountId] = useLocalStorage("accountId", "")
+
     const refetchInterval = 5000
-    const {data : inboxThreads} = api.mail.getNumThreads.useQuery({
+    const { data: inboxThreads } = api.mail.getNumThreads.useQuery({
         accountId,
-        tab : "inbox",
-    },{enabled : !!accountId && !!tab , refetchInterval})
-    const {data : draftsThreads} = api.mail.getNumThreads.useQuery({
-        accountId,
-        tab : "drafts",
-    }, {enabled : !!accountId && !!tab , refetchInterval})
+        tab: "inbox"
+    }, { enabled: !!accountId && !!tab, refetchInterval })
 
-    const {data : sentThreads} = api.mail.getNumThreads.useQuery({
+    const { data: draftsThreads } = api.mail.getNumThreads.useQuery({
         accountId,
-        tab : "sent",
-    },{enabled : !!accountId && !!tab , refetchInterval})
+        tab: "drafts"
+    }, { enabled: !!accountId && !!tab, refetchInterval })
 
-    console.log("inbox , drafts" , inboxThreads , draftsThreads);
+    const { data: sentThreads } = api.mail.getNumThreads.useQuery({
+        accountId,
+        tab: "sent"
+    }, { enabled: !!accountId && !!tab, refetchInterval })
+
     return (
-      <>
-        <Nav
-        isCollapsed = {isCollapsed}
-        links={[
-            {
-                title : "Inbox",
-                label : inboxThreads?.toString() || "0",
-                icon : Inbox,
-                variant: tab === "inbox" ? "default" : "ghost",
-            },
-            {
-                title : "Drafts",
-                label : draftsThreads?.toString() || "0",
-                icon : File,
-                variant: tab === "drafts" ? "default" : "ghost",
-            },
-            {
-                title : "Sent",
-                label : sentThreads?.toString() || "0",
-                icon : Send,
-                variant: tab === "sent" ? "default" : "ghost",
-            }
-        ]}
-        
-        />
-      </>
+        <>
+            <Nav
+                isCollapsed={isCollapsed}
+                links={[
+                    {
+                        title: "Inbox",
+                        label: inboxThreads?.toString() || "0",
+                        icon: Inbox,
+                        variant: tab === "inbox" ? "default" : "ghost",
+                    },
+                    {
+                        title: "Drafts",
+                        label: draftsThreads?.toString() || "0",
+                        icon: File,
+                        variant: tab === "drafts" ? "default" : "ghost",
+                    },
+                    {
+                        title: "Sent",
+                        label: sentThreads?.toString() || "0",
+                        icon: Send,
+                        variant: tab === "sent" ? "default" : "ghost",
+                    },
+                ]}
+            />
+        </>
     )
 }
 
-export default SideBar;
+export default SideBar
